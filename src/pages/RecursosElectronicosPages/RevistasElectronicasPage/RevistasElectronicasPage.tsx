8b6d914eeffd96845b2449bfa8f74d994664ebc4
BlueVelociraptor <bluevelociraptor1555@gmail.com>
2025-06-19 10:15:22 -0500

    Se agregaron las rutas restantes de Recursos electronicos y mejor orden en la carpeta Pages

---
+++ src/pages/RecursosElectronicosPages/RevistasElectronicasPage/RevistasElectronicasPage.tsx
@@ -0,0 +1,52 @@
+import { PageContainer, PageHeader, RecursosElectronicosCard, RecursosElectronicosGrid } from "../../../components"
+
+export const RevistasElectronicasPage = () => {
+    return (
+        <>
+            <PageHeader>Revistas Electrónicas</PageHeader>
+
+            <PageContainer>
+                <RecursosElectronicosGrid>
+                    <RecursosElectronicosCard
+                        title="Chicago Journal"
+                        description="Lorem ipsum dolor sit amet, consectetur"
+                        image="/Chicago_Journal.png"
+                        siteLink="https://www.journals.uchicago.edu/"
+                    />
+                    <RecursosElectronicosCard
+                        title="sciELO"
+                        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur mattis sit amet enim eget malesuada. Integer eleifend convallis ipsum ac blandit. Praesent euismod aliquam nisl, ut malesuada nulla accumsan eu. Fusce non felis facilisis, vulputate augue at, gravida elit. Phasellus quam lacus, finibus non molestie in, dapibus id ipsum. Maecenas purus sem, lacinia vel ipsum at, cursus faucibus velit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Proin elit purus, tincidunt vitae sagittis vitae, consectetur ac orci. Curabitur at pellentesque orci. Integer id posuere massa, eu dignissim sapien. Pellentesque vestibulum placerat velit, vel commodo lorem feugiat vitae. "
+                        image="/scielo.png"
+                        siteLink="https://www.journals.uchicago.edu/"
+                    />
+                    <RecursosElectronicosCard
+                        title="Redalyc"
+                        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur mattis sit amet enim eget malesuada. Integer eleifend convallis ipsum ac blandit. Praesent euismod aliquam nisl, ut malesuada nulla accumsan eu. Fusce non felis facilisis, vulputate augue at, gravida elit. Phasellus quam lacus, finibus non molestie in, dapibus id ipsum. Maecenas purus sem, lacinia vel ipsum at, cursus faucibus velit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Proin elit purus, tincidunt vitae sagittis vitae, consectetur ac orci. Curabitur at pellentesque orci. Integer id posuere massa, eu dignissim sapien. Pellentesque vestibulum placerat velit, vel commodo lorem feugiat vitae. "
+                        image="/redalyc.jpg"
+                        siteLink="https://www.journals.uchicago.edu/"
+                    />
+                    <RecursosElectronicosCard
+                        title="Redalyc"
+                        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur mattis sit amet enim eget malesuada. Integer eleifend convallis ipsum ac blandit. Praesent euismod aliquam nisl, ut malesuada nulla accumsan eu. Fusce non felis facilisis, vulputate augue at, gravida elit. Phasellus quam lacus, finibus non molestie in, dapibus id ipsum. Maecenas purus sem, lacinia vel ipsum at, cursus faucibus velit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Proin elit purus, tincidunt vitae sagittis vitae, consectetur ac orci. Curabitur at pellentesque orci. Integer id posuere massa, eu dignissim sapien. Pellentesque vestibulum placerat velit, vel commodo lorem feugiat vitae. "
+                        image="/redalyc.jpg"
+                        siteLink="https://www.journals.uchicago.edu/"
+                    />
+
+                    <RecursosElectronicosCard
+                        title="Redalyc"
+                        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur mattis sit amet enim eget malesuada. Integer eleifend convallis ipsum ac blandit. Praesent euismod aliquam nisl, ut malesuada nulla accumsan eu. Fusce non felis facilisis, vulputate augue at, gravida elit. Phasellus quam lacus, finibus non molestie in, dapibus id ipsum. Maecenas purus sem, lacinia vel ipsum at, cursus faucibus velit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Proin elit purus, tincidunt vitae sagittis vitae, consectetur ac orci. Curabitur at pellentesque orci. Integer id posuere massa, eu dignissim sapien. Pellentesque vestibulum placerat velit, vel commodo lorem feugiat vitae. "
+                        image="/redalyc.jpg"
+                        siteLink="https://www.journals.uchicago.edu/"
+                    />
+
+                    <RecursosElectronicosCard
+                        title="Redalyc"
+                        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur mattis sit amet enim eget malesuada. Integer eleifend convallis ipsum ac blandit. Praesent euismod aliquam nisl, ut malesuada nulla accumsan eu. Fusce non felis facilisis, vulputate augue at, gravida elit. Phasellus quam lacus, finibus non molestie in, dapibus id ipsum. Maecenas purus sem, lacinia vel ipsum at, cursus faucibus velit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Proin elit purus, tincidunt vitae sagittis vitae, consectetur ac orci. Curabitur at pellentesque orci. Integer id posuere massa, eu dignissim sapien. Pellentesque vestibulum placerat velit, vel commodo lorem feugiat vitae. "
+                        image="/redalyc.jpg"
+                        siteLink="https://www.journals.uchicago.edu/"
+                    />
+                </RecursosElectronicosGrid>
+            </PageContainer>
+        </>
+    )
+}
